9b1ed31087bfcdf0c47b1968513685c09226f7b7
kusiro <jackysoon871008@gmail.com>
2021-04-23 15:40:46 +0800

    107

--- Mid/10768.c
+++ Mid/10768.c
@@ -3,5 +3,12 @@
 int main(){
   int a, b, c;
   scanf("%d %d %d", &a, &b, &c);
-  
+  if(b % a == 0 && c % b == 0){
+    if(b / a == c / b){
+      printf("%d %d", a / (b / a), c * (b / a));
+    }
+  }
+  if(b - a == c - b){
+    printf("%d %d", a - (b - a), c + (b - a));
+  }
 }
